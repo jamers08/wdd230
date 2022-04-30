dcf1471c5d300860d42f63c04a25b52cfe787c96
jamers08 <sto20046@byui.edu>
2022-04-30 11:53:22 -0400

    font added

---
+++ js/lastModified.js
@@ -0,0 +1,6 @@
+let d = new Date(document.lastModified);
+const year = d.getFullYear();
+let x= "\u00A9";
+
+document.getElementById('year').textContent = x + year + " - Jamie Stott - South Carolina";
+document.getElementById('lastMod').textContent = d;
