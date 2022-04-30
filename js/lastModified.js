82ca811137ca2fa0058454c28fabda2e785d7fba
jamers08 <sto20046@byui.edu>
2022-04-30 15:39:02 -0400

    update

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,1 +1,7 @@
-document.querySelector('#updated').textContent=`Last updated: ${document.lastModified}`
+let d = new Date(document.lastModified);
+const year = d.getFullYear();
+let x= "\u00A9";
+
+document.getElementById('year').textContent = x + year + " - Jamie Stott - South Carolina";
+
+document.querySelector('#lastUpdate').textContent=`Last updated: ${document.lastModified}`
