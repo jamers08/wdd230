50859314e0bdf5efa3635c0ed5bfe1b2aaeeb3b7
jamers08 <sto20046@byui.edu>
2022-04-30 13:05:12 -0400

    fixed lastmod css

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,6 +1,1 @@
-let d = new Date(document.lastModified);
-const year = d.getFullYear();
-let x= "\u00A9";
-
-document.getElementById('year').textContent = x + year + " - Jamie Stott - South Carolina";
-document.getElementById('lastMod').textContent = d;
+document.querySelector('#updated').textContent=`Last updated: ${document.lastModified}`
