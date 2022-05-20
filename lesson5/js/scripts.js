4d5ecdc19bf8ac288e8e77b28eeb312ca8daaad7
jamers08 <sto20046@byui.edu>
2022-05-19 20:35:19 -0400

    halfway done

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,3 +1,24 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', function() {
+ const listAdd = input.value;
+ input.value = '';
+ 
+ const listItem = document.createElement('li');
+ const listDelete = document.createElement('button');
+
+ listItem.textContent = listAdd;
+ listDelete.textContent = ❌;
+
+ listItem.appendChild(listDelete);
+ list
+
+});
+
+//footer
+
 //Last modified
 document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
