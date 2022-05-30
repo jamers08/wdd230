b7126aacdf96de5a4f40bfdc0abfe1ff1965fb02
jamers08 <sto20046@byui.edu>
2022-05-30 02:21:49 -0400

    update

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -26,6 +26,7 @@ let d = new Date().getDay();
 const banner = document.getElementById("banner");
 if (d === 1 || d === 2) {
     banner.style.display = "block";
+
 }
 
 const close = document.querySelector("#close");
