18d26c8a1658931f4874cab1e17b4832eb2c73e2
jamers08 <sto20046@byui.edu>
2022-06-03 02:00:53 -0400

    update

---
+++ lesson7/js/scripts.js
@@ -0,0 +1,31 @@
+// get all images with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//optional parameters being set for the IntesectionalObserver
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+//first check to see if Intersection Observer is supported
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
