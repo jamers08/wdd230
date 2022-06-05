544b6f3702fd1f9718cd8d06f7e3b92f985655ab
jamers08 <sto20046@byui.edu>
2022-06-04 23:40:18 -0400

    upload

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -41,3 +41,35 @@ close.addEventListener('click', () => {
 document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
 document.getElementById("copyright").innerHTML = new Date().getFullYear();
+
+// get all images with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//optional parameters being set for the IntesectionalObserver
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+//first check to see if Intersection Observer is supported
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
