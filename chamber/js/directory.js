05e3a799905374e111dee3233fe43e500e1b95ee
jamers08 <sto20046@byui.edu>
2022-06-20 00:48:41 -0400

    upload

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,42 +1,14 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://jamers08.github.io/wdd230/chamber/json/data.json';
 const cards = document.querySelector('.cards');
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+async function getMembers() {
+  let response = await fetch(URL);
+  if (response.ok) {
+    let data = await response.json();
+    console.log(data);
+  } else {
+    throw Error(response.statusText);
+  }
+}
 
-    const prophets = jsonObject['prophets'];
-    
-    prophets.forEach(prophet => {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let logo = document.createElement('img');
-        let name = document.createElement('p');
-        let phone = document.createElement('p');
-        let website = document.createElement('p');
-        let address = document.createElement('p'):
-
-        h2.textContent = `${prophet.level}`;
-        name.textContent = `${prophet.name}`;
-        phone.textContent = `${prophet.phone}`;
-        website.textContent = `${prophet.website}`;
-        address.textContent = `${prophet.address}`;
-
-
-        logo.setAttribute('src', prophet.logo);
-        logo.setAttribute('alt', `Logo for ${prophet.name}`);
-        logo.setAttribute('loading', 'lazy');
-
-        card.appendChild(h2);
-        card.appendChild(logo);
-        card.appendChild(name);
-        card.appendChild(phone);
-        card.appendChild(website);
-        card.appendChild(address);
-
-        document.querySelector('div.cards').appendChild(card);
-    });
-  });
+getmembers();
