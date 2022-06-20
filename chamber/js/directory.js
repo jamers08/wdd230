f9e61ec18b0c5145e0b3b0bc3533208856685fdf
jamers08 <sto20046@byui.edu>
2022-06-20 02:07:18 -0400

    upload

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,14 +1,33 @@
-const requestURL = 'https://jamers08.github.io/wdd230/chamber/json/data.json';
+const URL = 'https://jamers08.github.io/wdd230/chamber/json/data.json';
 const cards = document.querySelector('.cards');
 
 async function getMembers() {
   let response = await fetch(URL);
   if (response.ok) {
     let data = await response.json();
-    console.log(data);
+    //console.log(data);
+    buildBusinessCards(data);
   } else {
     throw Error(response.statusText);
   }
 }
 
-getmembers();
+function buildBusinessCards(data) {
+  data.members.forEach(member => {
+    let card = document.createElement('section');
+    let h3 = document.createElement('h3');
+    let name = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
+    let address = document.createElement('p');
+    let logo = document.createElement('img');
+
+    h3.textContent = `${member.level}`;
+
+    card.append(h3);
+    
+   cards.append(card) ;
+  });
+}
+
+getMembers();
--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,10 +5,36 @@ async function getProphets() {
   let response = await fetch(URL);
   if (response.ok) {
     let data = await response.json();
-    console.log(data);
+    //console.log(data);
+    buildProphetCards(data);
   } else {
     throw Error(response.statusText);
   }
+}
+
+function buildProphetCards(data) {
+  data.prophets.forEach(prophet => {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birthdate = document.createElement('p');
+    let birthplace = document.createElement('p');
+    let portrait = document.createElement('img');
+
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+    
+    card.append(h2);
+    card.appendChild(birthdate);
+    card.appendChild(birthplace);
+    card.appendChild(portrait);
+
+    cards.append(card) ;
+  });
 }
 
 getProphets();
