b8130c41c77beb4f53470bbcfa41f189c310e1f0
jamers08 <sto20046@byui.edu>
2022-06-27 02:50:39 -0400

    css update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,8 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#wind-speed');
+const feelsLike = document.querySelector('#chill');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=Columbia&units=imperial&appid=b8aa599bead2268ad5027f753b37e704';
 
@@ -11,7 +13,7 @@ async function apiFetch() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        //console.log(data); // this is for testing the call
+        console.log(data); // this is for testing the call
         displayResults(data);
       } else {
           throw Error(await response.text());
@@ -20,8 +22,10 @@ async function apiFetch() {
 }
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-
+    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
+    windSpeed.innerHTML = `${weatherData.wind.speed.toFixed(0)}`;
+    feelsLike.innerHTML = `${weatherData.main.feels_like.toFixed(0)}`;
+    
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
     
@@ -29,3 +33,14 @@ function displayResults(weatherData) {
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
 }
+
+/*let t = currentTemp;
+let s = windSpeed;
+let output = document.getElementById('chill');
+
+if (t <= 50 && s > 3) {
+    let windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    output.textContent = Math.round(windChill);
+} else {
+    output.textContent = "N/A"
+}*/
