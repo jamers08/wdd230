291dd0b2a845780cb8ceafe0fd8eb22f31b292be
jamers08 <sto20046@byui.edu>
2022-06-27 01:14:18 -0400

    update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,16 +1,3 @@
-/*
-let t = 33;
-let s = 15;
-let output = document.getElementById('chill');
-
-if (t <= 50 && s > 3) {
-    let windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    output.textContent = Math.round(windChill);
-} else {
-    output.textContent = "N/A"
-}
-*/
-
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
