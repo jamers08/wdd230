39261721b7f04dcd2bedfbe02272855cf5cecf49
jamers08 <sto20046@byui.edu>
2022-07-02 22:09:52 -0400

    update

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -1,10 +1,8 @@
-var d = new Date();
+let d = new Date();
 
-// Set the value of the "date" field
 document.getElementById("date").value = d.toDateString();
 
-// Set the value of the "time" field
-var hours = d.getHours();
-var mins = d.getMinutes();
-var seconds = d.getSeconds();
+let hours = d.getHours();
+let mins = d.getMinutes();
+let seconds = d.getSeconds();
 document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
