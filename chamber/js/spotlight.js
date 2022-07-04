dfab694051fbcdf0de9d7f0a057526dc262e0a17
jamers08 <sto20046@byui.edu>
2022-07-04 00:56:34 -0400

    update

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,51 +1,53 @@
 const URL = 'https://jamers08.github.io/wdd230/chamber/json/data.json';
-const cards = document.querySelector('#spotlight');
+const spotlight = document.querySelector('#spotlight');
 
 fetch(URL)
-  .then(function(response) {
+  .then(function (response){
     return response.json();
   })
-  .then(function(jsonObject) {
+  .then(function (jsonObject) {
     const members = jsonObject['members'];
     let statusCheck = members.filter(members => (members.level == 'Gold' || members.level == 'Silver'));
-    //try array.map
+    //try array.map()?
     for (let i = 0; i < 3; i++) {
       shuffle = Math.floor(Math.random() * statusCheck.length);
-      buildSpotlightCards(statusCheck[shuffle]);
-      members = members.filter(members => (members.name != statusCheck[shuffle].name));
+      displaySpotlightCard(statusCheck[shuffle]);
+      statusCheck = statusCheck.filter(members => (members.name != statusCheck[shuffle].name));
     }
   });
 
+function displaySpotlightCard(members) {
+  //create elements from json--remember to look back and double check!!
+  let card = document.createElement('div');
+  let name = document.createElement('h2');
+  let logo = document.createElement('img');
+  let message = document.createElement('p');
+  let website = document.createElement('a');
+  let phone = document.createElement('p');
 
-function buildSpotlightCards(data) {
-  members.forEach(member => {
-    let card = document.createElement('div');
-    let h3 = document.createElement('h3');
-    let name = document.createElement('p');
-    let phone = document.createElement('p');
-    let website = document.createElement('a');
-    let address = document.createElement('p');
-    let logo = document.createElement('img');
+  //set attributes and text content
+  card.setAttribute('class', 'spot');
 
-    h3.textContent = `${member.level}`;
-    name.textContent = `${member.name}`;
-    phone.textContent = `${member.phone}`;
-    website.textContent = "Go to Website";
-    website.setAttribute('href', member.website);
-    address.textContent = `${member.address}`;
-    
-    logo.setAttribute('src', member.logo);
-    logo.setAttribute('alt', `Logo for ${member.name}`);
+  name.textContent = `${members.name}`;
 
-    card.append(h3);
-    card.appendChild(logo);
-    card.appendChild(name);
-    card.appendChild(phone);
-    card.appendChild(address);
-    card.appendChild(website);
-    
-   cards.append(card) ;
-  });
-}
+  logo.setAttribute('src', members.logo);
+  logo.setAttribute('alt', `Logo for ${members.name}`);
+  logo.setAttribute('loading', 'lazy');
 
-getMembers();
+  message.textContent = `${members.message}`;
+
+  website.setAttribute('href', members.website);
+  website.textContent = `Visit Website`;
+
+  phone.textContent = `${members.phone}`;
+
+  //append elements to new div
+  card.appendChild(name);
+  card.appendChild(logo);
+  card.appendChild(message);
+  card.appendChild(website);
+  card.appendChild(phone);
+
+  //append new div to spotlight
+  spotlight.appendChild(card);
+};
