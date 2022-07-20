5352642f1e44ba84cc05049f3ba8dfafedafdf3c
jamers08 <sto20046@byui.edu>
2022-07-20 17:30:42 -0400

    update

--- final/js/temples.js
+++ final/js/temples.js
@@ -16,87 +16,50 @@ async function getTemples() {
 
 function buildTempleCards(data) {
     data.temple.forEach(temple => {
-        let card = document.createElement('section');
-        let name = document.createElement('h2');
-        let pic = document.createElement('img');
-        let addressLabel = document.createElement('h3');
-        let phoneLabel = document.createElement('h3');
-        let phone = document.createElement('p');
-        let emailLabel = document.createElement('h3');
-        let email = document.createElement('p');
-        let servicesLabel = document.createElement('h3');
-        let services = document.createElement('p');
-        let historyLabel = document.createElement('h3');
-        let history = document.createElement('p');
-        let ordinanceLabel = document.createElement('h3');
-        let ordinances = document.createElement('p');
-        let sessionLabel = document.createElement('h3');
-        let sessions = document.createElement('p');
-        let closureLabel = document.createElement('h3');
-        let closures = document.createElement('p');
+        const card = document.createElement('section');
+        const name = document.createElement('h2');
+        const pic = document.createElement('img');
+        const addressLabel = document.createElement('h3');
+        const addresses = document.createElement('ul');
+        const phoneLabel = document.createElement('h3');
+        const phone = document.createElement('p');
+        const emailLabel = document.createElement('h3');
+        const email = document.createElement('p');
+        const servicesLabel = document.createElement('h3');
+        const services = document.createElement('ul');
+        const historyLabel = document.createElement('h3');
+        const history = document.createElement('p');
+        const ordinanceLabel = document.createElement('h3');
+        const ordinances = document.createElement('p');
+        const sessionLabel = document.createElement('h3');
+        const sessions = document.createElement('p');
+        const closureLabel = document.createElement('h3');
+        const closures = document.createElement('p');
 
         name.textContent = `${temple.name}`;
         
         addressLabel.textContent = `${temple.addressLabel}:`;
-        temple.address.forEach(address => {
-            const addy = document.createElement('p');
-            addy.textContent = fullAddress
-            card.appendChild(fullAddress);
+        temple.addresses.forEach(service => {
+            const serviceItem = document.createElement('li');
+            serviceItem.textContent = service;
+            addresses.appendChild(serviceItem);
         })
         
-        phoneLabel.textContent = `${temple.phoneLabel}:`;
-        phone.textContent = `${temple.phone}`;
-        emailLabel.textContent = `${temple.emailLabel}:`;
-        email.textContent = `${temple.email}`;
-        servicesLabel.textContent = `${temple.servicesLabel}:`;
-        historyLabel.textContent = `${temple.historyLabel}:`;
-        ordinanceLabel.textContent = `${temple.ordinanceLabel}`;
-        ordinances.textContent = `${temple.ordinances}`;
-        sessionLabel.textContent = `${temple.sessionLabel}`;
-        closureLabel.textContent = `${temple.closureLabel}`;
+        temple.services.forEach(service => {
+            const serviceItem = document.createElement('li');
+            serviceItem.textContent = service;
+            services.appendChild(serviceItem);
+        })
 
         pic.setAttribute('src', temple.pic);
         pic.setAttribute('alt', `Picture of ${temple.name}`);
         pic.setAttribute("loading", "lazy");
 
-
-        addressLine.textContent =`${temple.address}`;
-
-        servicesLine.textContent = `${temple.services}`;
-
-        history.textContent = `${temple.history}`;
-        history.setAttribute('class', 'history');
-        historyLine.textContent = `${temple.history[i].historySub} ${temple.history[i].historyDate}`;
-
-        sessions.textContent = `${temple.sessions}`;
-        sessions.setAttribute('class', 'sessions');
-        sessionLine.textContent = `${temple.sessions[i].sessionDay} ${temple.sessions[i].sessionTime}`;
-     
-        closures.textContent = `${temple.closures}`;
-        closures.setAttribute('class', 'closures');
-        closureLine.textContent = `${temple.closures[i].line}`;
-
         card.append(name);
         card.appendChild(pic);
         card.appendChild(addressLabel);
-
-        card.appendChild(phoneLabel);
-        card.appendChild(phone);
-        card.appendChild(emailLabel);
-        card.appendChild(email);
-        card.appendChild(servicesLabel);
-
-        card.appendChild(servicesLine);
-        card.appendChild(historyLabel);
-
-        card.appendChild(historyLine);
-        card.appendChild(ordinanceLabel);
-        card.appendChild(ordinances);
-        card.appendChild(sessionLabel);
-
-        card.appendChild(sessionLine);
-        card.appendChild(closureLabel);
-        card.appendChild(closureLine);
+        card.appendChild(address);
+        card.appendChild(services);
 
         cards.append(card);
         
