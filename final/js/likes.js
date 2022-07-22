aca1873533d63924387ff810ee67bca248e2eb62
jamers08 <sto20046@byui.edu>
2022-07-22 05:05:57 -0400

    update

--- final/js/likes.js
+++
@@ -1,8 +0,0 @@
-document.getElementById(`${temple[i]}like`).innerHTML = localStorage.likesforMosesLake + "Likes";
-function ${temple[i]}Count() {
-        if (typeof(Storage) !== "undefined") {
-            if (localStorage.`${temple[i]}likes`) {
-                localStorage
-            }           
-        }
-    }
